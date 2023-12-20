0b593f8d0e0fae7767162f633b3d1164d389b708
mariem-mezghani <118765518+mariem-mezghani@users.noreply.github.com>
2023-12-20 09:32:26 +0100

    Update pyspark_consumer_hive.py

--- pyspark_consumer_hive.py
+++ pyspark_consumer_hive.py
@@ -1,6 +1,9 @@
+#Import Required Libraries
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+
+#Create a Spark Session with Hive Support
 spark = SparkSession.builder \
     .appName("writetohive") \
     .config("spark.sql.warehouse.dir", "/user/hive/warehouse") \
@@ -9,28 +12,23 @@ spark = SparkSession.builder \
     .getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
 
-# Write to Hive using foreachBatch
+#Define Function to Write to Hive
 def write_to_hive(batch_df, batch_id):
     print(f"Processing batch: {batch_id}")
-    
-    # Select columns
     hive_df = batch_df.select("numbers", "contract_name", "banking", "bike_stands",
                                "available_bike_stands", "available_bikes", "address",
                                "status", "position", "timestamps")
 
-    # Print the first few rows for debugging
     print("Sample data in the batch:")
     hive_df.show()
-
-    # Write to Hive
     hive_df.write.saveAsTable(name="bikes_stations.bikes_stations", format="hive", mode='append')
     print("Data written to Hive successfully.")
 
-# Create the Hive database and use it
+#Create Hive Database and Use It
 spark.sql("CREATE DATABASE IF NOT EXISTS bikes_stations")
 spark.sql("USE bikes_stations")
 
-# Define the Hive table schema
+#Define Hive Table Schema
 hive_table_schema = """
     CREATE TABLE IF NOT EXISTS bikes_stations (
         numbers INT,
@@ -46,9 +44,10 @@ hive_table_schema = """
     )
 """
 
-# Create the Hive table
+#Create Hive Table
 spark.sql(hive_table_schema)
 
+#Define Schema for Kafka Data
 schema = StructType([
     StructField("numbers", IntegerType(), True),
     StructField("contract_name", StringType(), True),
@@ -65,6 +64,7 @@ schema = StructType([
     StructField("timestamps", StringType(), True),
 ])
 
+#Read Streaming Data from Kafka
 kafka_df = spark \
     .readStream \
     .format("kafka") \
@@ -73,21 +73,21 @@ kafka_df = spark \
     .option("startingOffsets", "latest") \
     .load()
 
+#Extract JSON Data and Modify Structure
 json_df = kafka_df.selectExpr("CAST(value AS STRING)") \
     .select(from_json("value", schema).alias("data")) \
     .select("data.*")
-
 json_df = json_df.withColumn("position", col("position").alias("position").cast("struct<lat:double, lng:double>"))
 
+#Filter Data
 zero_bikes_df = json_df.filter(col("available_bikes") == 0)
 
-# Write to Hive
+#Write Stream to Hive
 hive_query = json_df.writeStream \
     .outputMode("append") \
     .format("console") \
     .foreachBatch(write_to_hive) \
     .option("checkpointLocation", "/home/mariem/bike_stations_hive/checkpoints/new") \
     .start()
-
 hive_query.awaitTermination()
 
