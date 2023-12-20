be81a9057c89c46e28b2836a86d8adb85162b40d
mariem-mezghani <118765518+mariem-mezghani@users.noreply.github.com>
2023-12-20 09:28:19 +0100

    Update pyspark_consumer.py

--- pyspark_consumer.py
+++ pyspark_consumer.py
@@ -1,12 +1,17 @@
+#Import Required Libraries
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from elasticsearch import Elasticsearch
+
+#Creat an Elasticsearch Connection
 es = Elasticsearch("http://localhost:9200")
 
+#Creat a Spark Session
 spark = SparkSession.builder.appName("consumer").getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
 
+#Define Schema for the Data
 schema = StructType([
     StructField("numbers", IntegerType(), True),
     StructField("contract_name", StringType(), True),
@@ -23,6 +28,7 @@ schema = StructType([
     StructField("timestamps", StringType(), True),
 ])
 
+#Read Streaming Data from Kafka
 kafka_df = spark \
     .readStream \
     .format("kafka") \
@@ -31,21 +37,25 @@ kafka_df = spark \
     .option("startingOffsets", "latest")\
     .load()
 
+#Extract JSON Data
 json_df = kafka_df.selectExpr("CAST(value AS STRING)") \
     .select(from_json("value", schema).alias("data")) \
     .select("data.*")
+
+#Modify Data Structure
 json_df = json_df.withColumn("position", col("position").alias("position").cast("struct<lat:double, lon:double>"))
 
+#Filter Data
 zero_bikes_df = json_df.filter(col("available_bikes") == 0)
 
-
+#Write Stream to Console
 query = zero_bikes_df \
     .writeStream \
     .outputMode("append") \
     .format("console") \
     .start()
 
-
+#Write Stream to Elasticsearch
 data = json_df.writeStream \
         .format("org.elasticsearch.spark.sql") \
         .outputMode("append")\
